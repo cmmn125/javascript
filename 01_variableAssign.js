1d694ed0abaae5ec1075bfb1a8166004a354a5f2
cmmn125 <cmmn125@gmail.com>
2023-12-28 11:55:11 +0530

    01 variable assign
    01 variable assign

---
+++ 01_variableAssign.js
@@ -0,0 +1,27 @@
+var myFullName;
+console.log(myFullName);
+
+myFullName = "Chirag Mahajan";
+console.log("Full Name:", myFullName );
+
+myFullName = "Chirag Sanjay Mahajan";
+console.log("Complete Name", myFullName);
+
+ var myLove = "I love only JS";
+ console.log(myLove);
+
+ var firstName = "Chirag";
+ var lastName = "Mahajan";
+ var age = "27";
+ var address = "Nashirabad, jalgaon";
+ var pinCode = "425309";
+ var gender = "Male";
+ var isMarried = "No";
+
+ console.log("First Name:", firstName);
+ console.log("Last Name:", lastName);
+ console.log("Age:" , age);
+ console.log("Address:", address);
+ console.log("Pincode:", pinCode);
+ console.log("Gender:", gender);
+ console.log("Married:", isMarried);
---
+++ helloWord.js
@@ -0,0 +1,12 @@
+console.log("Hello JS , I am Chirag");
+var myName; //Variable Declaration
+console.log(myName);  //undefined
+
+myName = "Chirag";  //Variable Initialization
+console.log("First Name:",myName);
+
+var age ="27";    //Variable Declaration and Variable Initialization
+console.log("Age:",age);
+
+myName = "Chirag Mahajan";   //Variable Update
+console.log("Full Name:",myName);
