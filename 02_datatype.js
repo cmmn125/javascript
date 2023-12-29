aa3d443ed29ffc8caa56784149ff2a25afe4d39e
cmmn125 <cmmn125@gmail.com>
2023-12-29 18:55:08 +0530

    02 variableBasicsAssign
    02 variableBasicsAssign

---
+++ 02_datatype.js
@@ -0,0 +1,19 @@
+var firstName = "Chirag";
+var result = typeof firstName;
+console.log("Type of First name:", firstName);       // OR     console.log(typeof firstName);
+
+var age = 27;
+var result = typeof age;
+console.log("Type of Age:" , result);                  // OR   console.log(typeof age);
+
+var isMarried = true;
+var result = typeof isMarried;
+console.log("Type of isMarried:" , result);            // OR   console.log(typeof isMarried);
+
+var city;
+console.log(city);
+console.log("Type of City:", typeof city);
+
+var country = null;
+console.log(country);
+console.log("Type of Country:", typeof country);
---
+++ 02_variableBasicsAssign.js
@@ -0,0 +1,32 @@
+var mobNo = 7038007695;
+console.log("Mobile No:", mobNo, ",", typeof mobNo);
+
+var age = 27;
+console.log("Age:", age, " ," , typeof age);
+
+var pin = 425309;
+console.log("Pincode:", pin, "," , typeof pin);
+
+
+var myName = "Chirag";
+console.log("First Name:", myName , ",", typeof myName);
+
+var favCricketer = "virat";
+console.log("Fav Cricketer:", favCricketer, ",",typeof favCricketer);
+
+var favActor = "ranveer";
+console.log("Fav Actor:", favActor , ",",typeof favActor);
+
+
+var isMarried = true;
+console.log("Is Married:", isMarried, ",", typeof isMarried);
+
+var isSingle = false;
+console.log("Is Single:", isSingle , ", ", typeof isSingle);
+
+var  isGraduate = true;
+console.log("Is Graduate:", isGraduate ,",", typeof isGraduate);
+
+
+console.log("=======null vs undefined=======");
+console.log("null means an empty value and also a primitive type in Javascript.The variable which has been assigned as null contains no value.On the other hand Undefined means the variable has been declared,but its value not assigned.");
