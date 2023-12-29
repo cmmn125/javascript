2040dda586cc2410142f5f0265dfdb1d94f0c1bc
cmmn125 <cmmn125@gmail.com>
2023-12-29 21:11:36 +0530

    02 typeof
    02 typeof

---
+++ 02_typeOf.js
@@ -0,0 +1,27 @@
+"=======================Step1====================="
+
+var bankName;
+console.log(bankName, typeof bankName);
+bankName = 'SBI Bank';
+console.log(bankName, typeof bankName);
+
+"=======================Step2======================"
+
+var marks = '90%';
+console.log(marks, typeof marks);
+ 
+
+"========================Step3======================="
+
+var isWorking
+console.log(isWorking);
+isWorking = true;
+console.log(typeof isWorking);
+
+"=========================Step4========================"
+
+var totalCount = "Hundred and Seven";
+console.log(totalCount , typeof totalCount);
+
+totalCount = 107;
+console.log(totalCount, typeof totalCount);
