d3b77f0e2751bf766de4660ec99231bd10c4253e
cmmn125 <cmmn125@gmail.com>
2023-12-31 21:31:28 +0530

    03_string basic assign01
    03_string basic assign01

---
+++ 03_stringBasicAssign01.js
@@ -0,0 +1,26 @@
+function stringBasics(){
+    console.log("==========STEP1==========");
+
+var dreamCompany = "Google";
+console.log(`My dream company is: ${dreamCompany}`);
+
+console.log("==========STEP2==========");
+
+var hobby1 = "Playing Cricket";
+var hobby2 = "Reading Books";
+var hobby3 = "coding";
+
+console.log(`My Hobbies are: ${hobby1} , ${hobby2} , ${hobby3}`);
+
+console.log("==========STEP3==========");
+
+HOBBY1 = hobby1.length;
+HOBBY2 = hobby2.length;
+HOBBY3 = hobby3.length;
+
+console.log(HOBBY1+HOBBY2+HOBBY3);
+
+
+
+}
+stringBasics()
