69b4b3bae2317887daba976a2d1d8c837d7e7e5a
cmmn125 <cmmn125@gmail.com>
2024-01-03 17:19:14 +0530

    05 operator A
    05 operator A

---
+++ 05_operatorA.js
@@ -0,0 +1,49 @@
+console.log("==========STEP1==========");
+
+function squarOfWordLength(word){
+
+    word.length
+    var result = word.length * word.length;
+    
+    console.log(`Length of word is: ${word.length} ","  Square of length is: ${result}`); 
+
+}
+squarOfWordLength("JavaScript")
+
+function squarOfWordLength(word){
+    word.length
+    var res = word.length * word.length
+    
+    console.log(`Length of word is: ${word.length} ","  Square of length is: ${res}`); 
+
+}
+squarOfWordLength("Google Chrome")
+
+function squarOfWordLength(word){
+word.length
+var res = word.length * word.length
+
+console.log(`Length of word is: ${word.length} ","  Square of length is: ${res}`); 
+
+}
+squarOfWordLength("Developer Smart")
+
+console.log("==========STEP2==========");
+
+function word(){
+    str = "I am Angular Developer";
+    str.length
+    var res = str.length/4;
+    console.log(`Length of String is: ${str.length} ","  After division it is: ${res}`);
+
+    var result = str.length*4;
+    console.log(`Length of String is: ${str.length} ","  After multiplication it is: ${result}`);
+
+}
+word()
+
+
+
+
+
+
