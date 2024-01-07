0467b9352c691a08aaa2c42ae690b4b0a61b432f
cmmn125 <cmmn125@gmail.com>
2024-01-07 18:13:32 +0530

    06 while  loop
    06 while  loop

---
+++ 06_whileLoop.js
@@ -0,0 +1,36 @@
+
+
+console.log("=================Numbers from 1 to 10==============");
+
+var i = 1;
+while (i<=10) {
+console.log(i);
+i++;    
+}
+
+console.log("=================Numbers from 5 to 15==============");
+var i=5;
+while (i<=15) {
+    console.log(i);
+    i++;
+    
+}
+console.log("=================Numbers from 50 to 40==============");
+var i=50;
+while (i>=40) {
+    console.log(i);
+    i--;
+}
+
+console.log("=================First 10 Even Numbers==============");
+var i=2;
+while (i<=20) {
+    console.log(i);
+    i=i+2;
+}
+console.log("===============Write prog. to print the table of 5================");
+var i=5;
+while (i<=50) {
+    console.log(i);
+    i=i+5;
+}
