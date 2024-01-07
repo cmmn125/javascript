cac120597364158142b63fd8f02b65c5d98a1f43
cmmn125 <cmmn125@gmail.com>
2024-01-07 12:39:27 +0530

    06 for assign 01
    06 for assign 01

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,42 @@
+console.log("=================Numbers from 5 to 15==============");
+
+for (let index = 5; index <=15; index++) {
+    console.log(index);
+    
+}
+
+console.log("=================Numbers from 50 to 40==============");
+
+for (let index = 50; index >=40; index--) {
+    console.log(index);
+    
+}
+console.log("=================First 15 Odd Numbers==============");
+
+for (let index = 1; index <=30; index= index+2) {
+    console.log(index);
+    
+}
+console.log("=================First 10 Even Numbers==============");
+for (let index = 2; index <=20; index= index+2) {
+    console.log(index);
+    
+}
+console.log("===============Write prog. to print the table of 5================");
+
+ for (let index = 5; index <=50; index = index+5) {
+    console.log(index);
+    
+ }
+ console.log("===============Write prog. to print the table of 10================");
+
+ for (let index = 10; index <=100; index = index+10) {
+    console.log(index);
+    
+ }
+ console.log("===============Write prog. to print the table of 10 in reverse order================");
+for (let index = 100; index >=10; index = index-10) {
+    console.log(index);
+    
+}
+ 
