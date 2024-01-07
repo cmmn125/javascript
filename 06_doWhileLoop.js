e8ae5540297b82c60e3354b58e687b57d593acc1
cmmn125 <cmmn125@gmail.com>
2024-01-07 18:13:52 +0530

    06 dowhile loop
    06 dowhile loop

---
+++ 06_doWhileLoop.js
@@ -0,0 +1,33 @@
+
+
+
+console.log("=================Numbers from 1 to 10==============");
+var i = 1;
+do {
+    console.log(i);
+    i++;
+} while (i<=10);
+
+console.log("=================Numbers from 50 to 40==============");
+ i=50;
+do {
+    console.log(i);
+    i--;
+    
+} while (i>=40);
+
+console.log("=================First 10 Even Numbers==============");
+i=2;
+do {
+    console.log(i);
+    i= i+2;
+    
+} while (i<=20);
+
+console.log("===============Write prog. to print the table of 5================");
+i=5;
+do {
+    console.log(i);
+    i = i+5;
+    
+} while (i<=50);
