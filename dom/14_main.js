c0473c0c7b43c928119f4149f857ccf8a1ef209b
cmmn125 <cmmn125@gmail.com>
2024-01-26 17:29:44 +0530

    14 dom assign 2
    14 dom assign 2

---
+++ dom/14_main.js
@@ -0,0 +1,13 @@
+
+const listA = document.querySelector('#list')
+console.log(listA.innerHTML);
+
+listA.style.color = 'blue';
+
+const listB = document.querySelector('#heading')
+console.log(listB.innerHTML);
+
+listB.style.color = 'red';
+
+const project = document.querySelector('#Angular')
+project.innerHTML = 'This is angular project.'
